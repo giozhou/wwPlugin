d96645a3bb1139dfed08e5128ce7792a35bcd674
giozhou <gio.zhou@gmail.com>
2013-04-27 21:06:08 +0800

    set a release version

--- uomschat/uomschat/messaging.h
+++ uomschat/uomschat/messaging.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Wed Apr 24 19:54:48 2013
+/* at Sat Apr 27 20:28:29 2013
  */
 /* Compiler settings for messaging.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
